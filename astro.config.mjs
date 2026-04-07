cd32f7c2eddd0b7db8870c5bf8ed9883f0711a2b
Mohsen Mahmoodi <m.mahmoodi.biz@gmail.com>
2026-04-07 23:18:36 +0300

    Change site title

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Open Outbox',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://open-outbox.github.io' }],
 			sidebar: [
 				{
 					label: 'Guides',
