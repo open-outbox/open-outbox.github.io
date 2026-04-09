5eec084dfc11d48d3001e9e8c3a35939b4937d46
Mohsen Mahmoodi <m.mahmoodi.biz@gmail.com>
2026-04-09 21:30:42 +0300

    configure astro for github pages and update link rewriter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://open-outbox.github.io', 
+  	base: '/',
 	integrations: [
 		starlight({
 			title: 'Open Outbox',
